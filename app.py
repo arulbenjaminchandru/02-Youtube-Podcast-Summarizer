424bbee70fc0402208006eea39564434f34a1256
Arul Benjamin Chandru E <arulbenjaminchandru@gmail.com>
2024-07-04 20:18:30 +0530

    first commit

--- app.py
+++ app.py
@@ -4,14 +4,38 @@ import os
 from youtube_transcript_api import YouTubeTranscriptApi, TranscriptsDisabled
 import pysbd
 import dotenv
-import google.generativeai as genai
+from ibm_watsonx_ai.foundation_models.utils.enums import ModelTypes
+from ibm_watsonx_ai.metanames import GenTextParamsMetaNames as GenParams
+from ibm_watsonx_ai.foundation_models import ModelInference
 
 
 dotenv.load_dotenv()
 
-# Configure Gemini AI
-genai.configure(api_key=os.getenv("GEMINI_API_KEY"))
-model = genai.GenerativeModel("gemini-1.5-pro")
+def configure_watsonx():
+# Configure WatsonX Mistral AI Model
+    credentials = {
+        "url": "https://us-south.ml.cloud.ibm.com",
+        "apikey": os.getenv("WATSONX_API_KEY")
+    }
+    project_id = os.getenv("PROJECT_ID")
+    print([model.name for model in ModelTypes])
+
+    model_id = "mistralai/mixtral-8x7b-instruct-v01"
+
+    parameters = {
+        GenParams.DECODING_METHOD: "greedy",
+        GenParams.MIN_NEW_TOKENS: 1,
+        GenParams.MAX_NEW_TOKENS: 150
+    }
+
+    model = ModelInference(
+        model_id=model_id, 
+        params=parameters, 
+        credentials=credentials,
+        project_id=project_id)
+    
+    return model
+
 
 seg = pysbd.Segmenter(language='en', clean=True)
 
@@ -57,15 +81,13 @@ def chunk_large_text(text_list: list, max_size: int) -> list[str]:
     return txts
 
 def summarize_large_text(text_list: list, max_size: int) -> str:
+    summaries = ""
     txts = chunk_large_text(text_list, max_size)
-    prompt = "Summarize the following content"
-    summaries = prompt.join(txts)
-    response = model.generate_content(summaries)
-    print(response.text)
-    return response.text
+    summaries = summaries.join(txts)
+    return summaries
 
 # Streamlit UI
-st.title('YouTube Transcript Summarizer')
+st.title('YouTube Transcript Summarizer using watsonx Mistral 7B')
 url = st.text_input('Enter YouTube URL')
 submit = st.button('Submit')
 
@@ -76,8 +98,15 @@ if submit and url:
         if transcript:
             text_list = seg.segment(transcript)
             summary = summarize_large_text(text_list, max_size=2048)
-            st.subheader('Summary')
+            st.subheader("Extracted Text from Video")
             st.write(summary)
+            if summary:
+                model = configure_watsonx()
+                prompt = "Summarize the following content within 150 tokens : "
+                summary = prompt + "\n" + summary
+                response = model.generate(summary)
+                st.subheader('Summary')
+                st.write(response.get('results')[0]['generated_text'])
         else:
             st.write("No transcript found for this video.")
     else:
